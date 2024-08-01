262a65be345a5793faea0a59266ccd3172d20643
darnisha patel <darnisha2910@gmail.com>
2024-08-01 14:37:56 +0530

    added customize design functionality

---
+++ src/app/configure/design/page.tsx
@@ -0,0 +1,29 @@
+import { db } from "@/db"
+import { notFound } from "next/navigation"
+import DesignConfigurator from "./DesignConfigurator"
+
+interface pageProps {
+searchParams :{
+  [key:string]: string | string[] | undefined
+}
+}
+
+const page = async({searchParams}: pageProps) => {
+   const {id} = searchParams
+
+   if(!id || typeof id !== "string") {
+    return notFound()
+   }
+
+   const configuration = await db.configuration.findUnique({
+    where : {id},
+   })
+
+   if(!configuration) {
+    return notFound()
+   }
+   const {imageUrl,width,height} = configuration
+  return <DesignConfigurator configId = {configuration.id} imageUrl = {imageUrl} imageDimensions = {{width,height}} />
+}
+
+export default page
